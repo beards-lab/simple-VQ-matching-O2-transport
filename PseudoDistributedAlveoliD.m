91fb6b10b1515ee5d7c6434eeff388b8ff60cbb3
Jezek <jezekf@gmail.com>
2023-05-10 16:34:54 +0200

    Drafting pseudodistributed alveoli

--- PseudoDistributedAlveoliD.m
+++ PseudoDistributedAlveoliD.m
@@ -21,28 +21,10 @@ end
 % Pvdist = interp1(HbDisC, HbDisP,Cx , "linear");
 % plot(HbDisP, HbDisC, HbDisP, Ceq, '--', Pvdist, Cx, 'x-', 'Linewidth', 2);
 
-%% prepare random distribution
-N = 100;
-SD = 2e-1;
-rng(3); % set the random generator seed 
-Qdist = SD*randn(N, 1) + 1; % random vector for flow distribution
-Qdist = Qdist(find(Qdist > 0)); % select only non-zero flows
-Qdist = (Qdist) / mean(Qdist); % rescale to match total flow
-
-Vdist = SD*randn(N, 1) + 1; % random vector for ventilation
-Vdist = Vdist(find(Vdist > 0)); % select only non-zero ventilation
-Vdist = (Vdist) / mean(Vdist); % rescale to match total ventilation
-
-Qpb = ones(N, 1); % probability density "weight" multiplier
-Vpb = ones(N, 1); % probability density "weight" multiplier
-clf;plot(Qdist, Vdist, '.'); xlabel('Q');ylabel('V');
-% histogram(rnd, N/10)
-% subplot(121);histogram(rnd, 'FaceAlpha',0.2);
-
-
 %% Prepare pseudo-random continuous distribution
 N = 20;
-in = 0:1/N:2;
+SD = 2e-1;
+in = linspace(0, 2, N);
 % normal probability distribution density
 bell = normpdf(in, 1, SD);
 
@@ -50,16 +32,16 @@ bell = normpdf(in, 1, SD);
 trashold = 0.01;
 in = in(bell>trashold);
 bell = bell(bell>trashold);
+N = numel(bell);
+% bell2 = (bell) / sum(bell); % sums to one
 
-Qdist = in;
-Qpb = bell;
+Vdist = in/ mean(in); % ventilation sweep with mean of 1
+Pd = bell / mean(bell); % probability distribution with mean of one
+% sum
 
-% Vdist = 
-clf;
-plot(in, bell, '*');
-
-
-
+% plot(Qdist, Qpd, Qdist, bell2)
+figure;plot(Vdist, '*');
+plot(Pd, '*');
 
 drawPlots = true;
 if drawPlots
@@ -70,8 +52,8 @@ if drawPlots
 end
 
 %% Prepare inputs
-CO = 3:1:20;
-Vp = 3:1:20;
+CO = [5];
+V = [5];
 
 % CO = [10];
 % Vp = [10];
@@ -82,12 +64,12 @@ Qs = 5; % flow snapshot
 Vps = 5; % Ventilation snapshot
 
 % filename = sprintf("Data/DistributedModel_SD_%1.1e.mat", SD)
-filename = sprintf("Data/DistributedModelQV_SD_%1.1e.mat", SD)
+filename = sprintf("Data/PseudoDistributedModelQV_SD_%1.1e.mat", SD)
 
 %% get all the param shift tigerther
 
 %%% parameters
-D     = 380;      %apparent diffusion (L/min)
+D     = 285;      %apparent diffusion (L/min)
 Pair  = 150;    %atmospheric oxygen partial pressure (mmHg)
 Pin   = 45;     %mixed venous oxygen partial pressure - pulmonary inlet (mmHg)
 Vvasc = 1;      %volume of vascular space (ml)
@@ -101,94 +83,117 @@ D = DDp;
 
 par = [D Pair Pin alpha beta l];
 
-%% Run ModelD eval - test
+%% Run ModelD eval - test the distribution
 NN = 500;
-tic
-Qt = 0.05;Vpt = 0.3;
-par(6) = 1;
-par(1) = D;
-[Pv,Pa,Cvi,Pvi,EPSv,dx] = modelD_SS_relaxation(NN,par,HbDisP,HbDisC,Vps,Qs);
-t = toc;
-fprintf('At Q = %2.2f and Vp  = %2.2f, pO2_{dist} = %2.1f (alv. = %2.1f), in %2.0f ms \n', Qt, Vpt, Pv, Pa, t*1000);
-
-NNN = 2;
-Cvid = zeros(NNN, 1);
-for i = 1:NNN
-    par(1) = D/NNN;
-    [~,~,C] = modelD_SS_relaxation(NN,par,HbDisP,HbDisC,Vps/NNN,Qs/NNN);
-    Cvid(i) = C(end);
-end
-Csum = sum(Cvid*Qs/NNN)/Qs;
-
-t = toc;
-fprintf('Cvi %1.3f, CviSum %1.3f \n', Cvi(end), Csum);
-
+% tic
+% Qt = 0.05;Vpt = 0.3;
+% par(6) = 1;
+% par(1) = D;
+% [Pv,Pa,Cvi,Pvi,EPSv,dx] = modelD_SS_relaxation(NN,par,HbDisP,HbDisC,Vps,Qs);
+% t = toc;
+% fprintf('At Q = %2.2f and Vp  = %2.2f, pO2_{dist} = %2.1f (alv. = %2.1f), in %2.0f ms \n', Qt, Vpt, Pv, Pa, t*1000);
+% 
+% NNN = 3;
+% Cvid = zeros(NNN, 1);
+% for i = 1:NNN
+%     par(1) = D/NNN;
+%     [~,~,C] = modelD_SS_relaxation(NN,par,HbDisP,HbDisC,Vps/NNN,Qs/NNN);
+%     Cvid(i) = C(end);
+% end
+% Csum = sum(Cvid*Qs/NNN)/Qs;
+% 
+% t = toc;
+% fprintf('Cvi %1.3f, CviSum %1.3f \n', Cvi(end), Csum);
+% 
 
 
 %% init loop
 
-pVascND = zeros(length(CO), length(Vp)); % non distributed
-pAlvND = zeros(length(CO), length(Vp)); % non distributed
-Pvdist = zeros(length(CO), length(Vp)); % distributed
+pVascND = zeros(length(CO), length(V)); % non distributed
+pAlvND = zeros(length(CO), length(V)); % non distributed
+Pvdist = zeros(length(CO), length(V)); % distributed
 Pds = 0;Qds = 0;Ps = 0;
 params = par;
-pfs = @(snap, V, Q) save(sprintf('Data\snapshot_V%d_Q%d.mat', V, Q), "snap"); % paralel for snapshot
+pfs = @(snap, V, Q) save(sprintf('Data\\snapshot_V%d_Q%d.mat', V, Q), "snap"); % paralel for snapshot
 COEl = 1:length(CO);
-VpEl = 1:length(Vp); % VpElements
+VpEl = 1:length(V); % VpElements
 
-parfor iq = COEl % iterate flow
+for iv = VpEl % iterate ventilation
     par = params;
 
-    disp("Jedu " + num2str(round(iq/length(CO)*100)))
-    qrs = CO(iq)/N * Qdist; % random flow set
+    disp("Jedu " + num2str(round(iv/length(V)*100)))
+    vrs = V(iv)/N * Vdist; % pseudorandom ventilation set
+    
+
     Cvi = 0;Pvi = 0;
 
-    for iv = VpEl % iterate ventilation
-        vrs = Vp(iv)/N *Vdist; % ventilation random set
-        % vrs = ones(size(qrs))*Vp(iv)/N; % start simple - equal ventilation
+    for iq = COEl % iterate flow
+        qrs = CO(iq)/N .* ones(N, 1); % flow - start simple unmatched
+        % qrs = CO(iq)/N .* Vpd(iv)/N ; % flow matches ventilation 1:1
+        % qrs as a function of q
         
         % equal Q and Vp in all alveoli
         par(1) = D;
-        [pv,~, ~, ~] = modelD_SS_relaxation(NN,par,HbDisP,HbDisC,Vp(iv),CO(iq)); 
-        pVascND(iq, iv) = pv;
+        [pv1,~, ~, ~] = modelD_SS_relaxation(NN,par,HbDisP,HbDisC,V(iv),CO(iq)); 
+        pVascND(iq, iv) = pv1;
         
         tic
         % distribution within lungs
-        c = zeros(size(qrs));
-        p = zeros(size(qrs));
-        for iqs = 1:length(qrs) % iterate submodel flows
+        c = zeros(size(vrs));
+        p = zeros(size(vrs));
+        for iqs = 1:length(vrs) % iterate submodel flows
             par(1) = D/N;
             [~,~, Cvi, Pvi] = modelD_SS_relaxation(NN,par,HbDisP,HbDisC,vrs(iqs),qrs(iqs)); %loop through q and v and store fixed points
             c(iqs) = Cvi(end); % pulmonary end-capillary concentration
             p(iqs) = Pvi(end); % pulmonary end-capillary pO2
         end
         t = toc;
-        Q = sum(qrs); % sum of flows
-        Ca = sum(c.*qrs)/Q; % weighted average of concentrations by flow
-        Pvdist(iq, iv) = interp1(HbDisC, HbDisP,Ca , "linear"); % Pulmonary venous distributed sum
-        fprintf('At Q = %2.1f (%2.1f) and Vp  = %2.1f, pO2_{dist} = %2.1f (single comp. = %2.1f), in %2.0f ms (Ctrl %1.3e)\n', Q, CO(iq), Vp(iv), Pvdist(iq, iv), pv, t*1000, CO(iq)-Q);
-        if CO(iq) == Qs && Vp(iv) == Vps, 
-            % snapshot
-            snapshot = struct();
-            snapshot.Pds = p; % PO2 distribution snapshot
-            snapshot.Qds = qrs; % flow distribution snapshot
-            snapshot.Vds = vrs; % ventilation distribution snapshot
-            snapshot.Cds = c;
-            snapshot.Pvs = Pvdist(iq, iv); % venous pO2 snapshot
-            snapshot.PvNds = pv; % venous pO2 non-distributed snapshot
-            pfs(snapshot, Qs, Vps);
-        end
+        Peld = interp1(HbDisC, HbDisP,c , "linear"); % partial pressure per ventilation element
+        Cvd = (c.*qrs'.*Pd); % concentration venous distributed, weighted by "probability distribution"
+        % Pvd = interp1(HbDisC, HbDisP,Cvd , "linear"); 
+        Cv = sum(Cvd)/sum(qrs); % weighted average of concentrations by flow
+        % just another mean of calculation of the previous
+        % Cv = sum(1/N*c.*qrs'.*Pd)/mean(qrs); 
+        pv = interp1(HbDisC, HbDisP,Cv , "linear"); % Pulmonary venous distributed sum
+        Pvdist(iq, iv) = pv;
+        fprintf('At Q = %2.1f (%2.1f) and Vp  = %2.1f, pO2_{dist} = %2.1f (single comp. = %2.1f), in %2.0f ms (Ctrl %1.3e)\n', sum(qrs), CO(iq), Vpd(iv), Pvdist(iq, iv), pv1, t*1000, CO(iq)-sum(qrs));
+
+        %% plot that
+        figure(1);clf;subplot(211);
+        bar(vrs, Vdist.*Pd/N);title(sprintf('Distribution of ventilation among %d elements', N));
+        xlabel('Ventilated elements (L/)');
+
+        % distribution of flow
+        figure(2);clf;
+        bar(vrs, qrs);title('Distribution of flow');
+
+        % distribution of concentrations
+        % figure(3);clf;
+        % bar(vrs, c);title('Distribution of concentration (raw)');
+
+        % distribution of partial pressure
+        figure(3);clf;
+        bar(vrs, Peld);title('Distribution of partial pressures (raw)');
+
+        % Total partial pressure
+        figure(4);clf;hold on;
+        bar(vrs, Pvd);title(sprintf('Weighted partial pressures, total: %0.2f', Pvdist(iq, iv)));
+        plot(xlim, [pv1 pv1], 'r--')
+        plot(xlim, [pv pv], 'c:', 'LineWidth',1.5)
+        legend('Capillary pO2', '1 comp pO2', 'dist venous pO2', 'Location','northwest')
+        
     end
 end
-% save results
+%% save results
 DMR = struct(); % distributed model results
 DMR.CO = CO; % cardiac output
-DMR.Vp = Vp; % ventilation
+DMR.Vp = Vpd; % ventilation
 DMR.Pv = Pvdist; % PO2 in pulmonary veins
-
+DMR.Qpd = Qpd; % flow probability distribution - same for all data points
 DMR.pVascND = pVascND; % non distributed
 DMR.pAlvND = pAlvND;
-DMR.snap = load('Data\snapshot.mat').snap; % paralel for snapshot
+snapfile = sprintf('Data\\snapshot_V%d_Q%d.mat', Vps, Qs);
+DMR.snap = load(snapfile).snap; % paralel for snapshot
 
 save(filename, "DMR");
 %% load results
@@ -205,17 +210,28 @@ DMR = load(filename).DMR; % distributed model results
 % DMR.snap.Pvs = DMR.snap.Ps;
 % subplot(131); histogram(dmr.snap.Qds, N, "EdgeAlpha",0.2);
 % subplot(132); histogram(dmr.snap.Pds, N); 
+%%
 subplot(231); cla; hold on;
-plot(DMR.snap.Qds,DMR.snap.Vds, 'x');
-title(['Blood flow distribution in alveoli, Q_{total} = ' num2str(round(sum(DMR.snap.Qds), 2)) ' L/min']);
-xlabel('Q');ylabel('V');
-Qsm = mean(DMR.snap.Qds);
-Vsm = mean(DMR.snap.Vds);
-plot([Qsm Qsm], [min(DMR.snap.Vds) max(DMR.snap.Vds)], 'r--')
-plot([min(DMR.snap.Qds) max(DMR.snap.Qds)], [Vsm Vsm], 'r--')
-
+% b = DMR.snap.Qds.*DMR.Qpd;
+bar(DMR.snap.Qds, DMR.Qpd/sum(DMR.Qpd)*100);
+title(sprintf('Flow pseudodistribution at N = %d', N));
+ylabel('Count (%)');xlabel('BLood flow');
+% plot(DMR.snap.Qds,DMR.snap.Vds, 'x');
+% title(['Blood flow distribution in alveoli, Q_{total} = ' num2str(round(sum(DMR.snap.Qds), 2)) ' L/min']);
+% xlabel('Q');ylabel('V');
+% Qsm = mean(DMR.snap.Qds);
+% Vsm = mean(DMR.snap.Vds);
+% plot([Qsm Qsm], [min(DMR.snap.Vds) max(DMR.snap.Vds)], 'r--')
+% plot([min(DMR.snap.Qds) max(DMR.snap.Qds)], [Vsm Vsm], 'r--')
+%%
 subplot(232); cla;hold on;
 % plot(DMR.snap.Pds,1:numel(DMR.snap.Qds), 'x'); 
+b = DMR.snap.Qds.*DMR.Qpd;
+bar(DMR.snap.Vds, DMR.Qpd/sum(DMR.Qpd)*100);
+title(sprintf('Flow pseudodistribution at N = %d', N));
+ylabel('Count (%)');xlabel('BLood flow');
+
+
 histogram(DMR.snap.Pds, N/10);
 title(['Partial pressure in distributed alveoli, Pa = ' num2str(round(DMR.snap.Pvs, 2))]);
 plot([DMR.snap.PvNds DMR.snap.PvNds], [0 max(ylim)], 'c:', 'LineWidth',1.5)
